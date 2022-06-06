c27edf792d704293d8eb92066b4a62ec2468dee3
Anthony Hoang <anthonyhoang3074@gmail.com>
2022-06-06 10:55:14 -0500

    Added simple Ingredient script to Project

---
+++ Assets/Scripts/PoisonVial/Ingredient.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+
+public class Ingredient
+{
+    // Private instance variables
+    private float[] statProbabilities;
+
+    private const int NUM_STATS_CONTRIBUTED = 2;
+    private const int NUM_STATS_TOTAL = 4;
+
+    private const int POTENCY_INDEX = 0;
+    private const int POISON_INDEX = 1;
+    private const int REACTIVITY_INDEX = 2;
+    private const int STICKINESS_INDEX = 3;
+
+
+    // Main constructor for an ingredient
+    //  Pre: all stat chances added must be greater than 0f
+    //  Post: Ingredient object has been constructed
+    public Ingredient(float pot, float poi, float r, float s) {
+        Debug.Assert(pot >= 0f && poi >= 0f && r >= 0f && s >= 0f);
+
+        // Get total to normalize probability
+        statProbabilities = new float[NUM_STATS_TOTAL];
+        float total = pot + poi + r + s;
+
+        // Normalize probability so that all of the chances sum up to 1.0f
+        statProbabilities[POTENCY_INDEX] = pot / total;
+        statProbabilities[POISON_INDEX] = poi / total;
+        statProbabilities[REACTIVITY_INDEX] = r / total;
+        statProbabilities[STICKINESS_INDEX] = s / total;
+    }
+
+
+    // Main function to craft an ingredient
+    //  Pre: none
+    //  Post: returns a dictionary that maps Poison Vial stats to num of stat gains
+    public Dictionary<string, int> calculateStatGains() {
+        // Initialize dictionary
+        Dictionary<string, int> statGains = new Dictionary<string, int>();
+        statGains.Add("Potency", 0);
+        statGains.Add("Poison", 1);
+        statGains.Add("Reactivity", 2);
+        statGains.Add("Stickiness", 3);
+
+        // Add to stat buffs by rolling the dice twice
+        for (int i = 0; i < NUM_STATS_CONTRIBUTED; i++) {
+            float diceRoll = Random.Range(0f, 1f);
+            int currentStat = 0;
+
+            // Go down the list of probabilities to see if you get current stat
+            while (currentStat < statProbabilities.Length && diceRoll > statProbabilities[currentStat]) {
+                diceRoll -= statProbabilities[currentStat];
+                currentStat++;
+            }
+
+            Debug.Assert(currentStat < statProbabilities.Length);
+
+            // Increment dictionary
+            statGains["Potency"] += (currentStat == POTENCY_INDEX) ? 1 : 0;
+            statGains["Poison"] += (currentStat == POTENCY_INDEX) ? 1 : 0;
+            statGains["Reactivity"] += (currentStat == POTENCY_INDEX) ? 1 : 0;
+            statGains["Stickiness"] += (currentStat == POTENCY_INDEX) ? 1 : 0;
+        }
+
+        // Return dictionary
+        Debug.Assert(statGains.ContainsKey("Potency") && statGains.ContainsKey("Poison") && statGains.ContainsKey("Reactivity") && statGains.ContainsKey("Stickiness"));
+        Debug.Assert(statGains["Potency"] + statGains["Poison"] + statGains["Reactivity"] + statGains["Stickiness"] == NUM_STATS_CONTRIBUTED);
+        return statGains;
+    }
+
+
+    // Main function to present stat chances
+    //  Pre: probDisplays.Length == 4 and match in this order [Potency, Poison, Reactivity, Stickiness]
+    //  Post: update probability displays to show the probability chance of getting stat contributed
+    public void displayStatChances(INumberDisplay[] probDisplays) {
+        Debug.Assert(probDisplays.Length == NUM_STATS_TOTAL);
+
+        for (int i = 0; i < NUM_STATS_TOTAL; i++) {
+            int percentChance = Mathf.RoundToInt(statProbabilities[i] * 100f);
+            probDisplays[i].displayNumber(percentChance);
+        }
+    }
+
+}
