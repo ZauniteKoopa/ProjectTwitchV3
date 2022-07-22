7481373f80fb60b4009c8e27bd4f6e0142a0594c
Anthony Hoang <anthonyhoang3074@gmail.com>
2022-07-22 11:27:03 -0500

    Boiling blast implemented

---
+++ Assets/Scripts/AttackBehavs/PoisonMeleeAttack.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonMeleeAttack : MonoBehaviour
+{
+    private IVial poison;
+    private HashSet<ITwitchUnitStatus> hit = new HashSet<ITwitchUnitStatus>();
+    private float meleeAttackDuration = 0.3f;
+
+
+    // On awake, start melee attack sequence
+    private void Awake() {
+        StartCoroutine(meleeAttackSequence());
+    }
+
+    // Main melee attack sequence
+    private IEnumerator meleeAttackSequence() {
+        gameObject.SetActive(true);
+        yield return new WaitForSeconds(meleeAttackDuration);
+        gameObject.SetActive(false);
+        hit.Clear();
+    }
+
+
+    // Main function to connect basic attack damage to this new poison
+    //  Pre: newPoison CAN be null or non-null
+    //  Post: poison vial is now connected to this attack to calculate damage. Damage calculations depend on the basic attack instance
+    public void setVialDamage(IVial newPoison) {
+        poison = newPoison;
+    }
+
+
+    // On TriggerEnter
+    private void OnTriggerEnter(Collider collider) {
+        ITwitchUnitStatus colliderHurtbox = collider.GetComponent<ITwitchUnitStatus>();
+
+        if (colliderHurtbox != null && !hit.Contains(colliderHurtbox)) {
+            hit.Add(colliderHurtbox);
+            colliderHurtbox.poisonDamage(poison.getBoltDamage(0), poison, 1);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerPackage/PlayerAttack/ShotgunPivot.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunPivot : MonoBehaviour, ITwitchBasicAttack
+{
+    [SerializeField]
+    private PoisonMeleeAttack poisonBlast;
+    private float blastDuration = 0.35f;
+
+
+    // Main function to connect basic attack damage to this new poison
+    //  Pre: newPoison CAN be null or non-null
+    //  Post: poison vial is now connected to this attack to calculate damage. Damage calculations depend on the basic attack instance
+    public void setVialDamage(IVial newPoison) {
+        poisonBlast.setVialDamage(newPoison);
+    }
+
+
+    // Main function to fire the basic attack towards a certain direction
+    //  Pre: projDir is the direction that the projectile is facing and projSpeed is how fast the projectile goes
+    //  Post: sets up basic attack to go a certain direction
+    public void setUpMovement(Vector3 projDir, float projSpeed) {
+        transform.forward = projDir;
+        StartCoroutine(blastSequence());
+    }
+
+
+    // Blast sequence
+    private IEnumerator blastSequence() {
+        yield return new WaitForSeconds(blastDuration);
+        Object.Destroy(gameObject);
+    }
+
+
+
+    // Main function to get access to the transform of this component
+    //  Pre: none
+    //  Post: returns the transform associated with this object
+    public Transform getTransform() {
+        return transform;
+    }
+}
